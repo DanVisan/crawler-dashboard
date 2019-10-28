6409ce48cf00bef39060fd6edba9a41f814380a4
Dan Visan <visan.dan.constantin@gmail.com>
2019-10-28 18:30:32 +0200

    repository service and controllers

--- src/main/java/ro/vis/crawlerdashboard/controllers/BlacklistedKeywordsController.java
+++ src/main/java/ro/vis/crawlerdashboard/controllers/BlacklistedKeywordsController.java
@@ -1,8 +1,18 @@
 package ro.vis.crawlerdashboard.controllers;
 
 import org.springframework.stereotype.Controller;
+import ro.vis.crawlerdashboard.services.BlacklistedKeywordService;
 
 @Controller
 public class BlacklistedKeywordsController {
+    private final BlacklistedKeywordService service;
 
+    public BlacklistedKeywordsController(BlacklistedKeywordService service) {
+        this.service = service;
+    }
+
+    public void findAll() {
+        var all = service.findAll();
+        System.out.println(all);
+    }
 }
--- src/main/java/ro/vis/crawlerdashboard/services/BlacklistedKeywordService.java
+++ src/main/java/ro/vis/crawlerdashboard/services/BlacklistedKeywordService.java
@@ -1,8 +1,22 @@
 package ro.vis.crawlerdashboard.services;
 
 import org.springframework.stereotype.Service;
+import ro.vis.crawlerdashboard.entities.BlacklistedKeyword;
+import ro.vis.crawlerdashboard.repositories.BlacklistedKeywordRepository;
+
+import java.util.List;
 
 @Service
 public class BlacklistedKeywordService {
+
+    private final BlacklistedKeywordRepository repository;
+
+    public BlacklistedKeywordService(BlacklistedKeywordRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<BlacklistedKeyword> findAll() {
+        return repository.findAll();
+    }
 
 }
